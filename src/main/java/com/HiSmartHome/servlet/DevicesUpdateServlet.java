5f61ace7ec5dda06b2e784f144b49bf88c5d93f4
superH-text <108380766+superH-text@users.noreply.github.com>
2023-12-14 22:56:05 +0800

    Devices servlet的建立

---
+++ src/main/java/com/HiSmartHome/servlet/DevicesUpdateServlet.java
@@ -0,0 +1,19 @@
+package com.HiSmartHome.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "DevicesUpdateServlet", value = "/DevicesUpdateServlet")
+public class DevicesUpdateServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
